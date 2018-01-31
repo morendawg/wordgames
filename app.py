5ae051d422a27939135865a512970640c706029d
morenod <dmorenocuellar@gmail.com>
2018-01-31 14:36:23 -0500

    added task_1 final strings

--- app.py
+++ app.py
@@ -11,32 +11,22 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-def generate_scrambled(num):
-	alphabet = string.ascii_lowercase
-	strLength = 6
+def parse_scrambled(file):
+	file = open(file, "r")
+	lines = [line.rstrip('\n') for line in file]
 	entries = []
-	for x in range (0, num) :
-		swaps = numpy.random.choice(numpy.arange(0,2), p=[0.07, 0.93])
-
-		i = random.randint(0, 26-strLength)
-
-		chunk = alphabet[i : i+strLength]
-
-		for x in range (0, swaps) :
-			i = random.randint(0, strLength-2)
-			j = i +1
-			chunk = list(chunk)
-			chunk[i], chunk[j] = chunk[j], chunk[i]
-			chunk = ''.join(chunk)
-
-		print "{} swaps -> {}".format(swaps, chunk)
-		entries.append(chunk)
-	return entries
+	answers = []
+	for line in lines:
+		split = line.split(',')
+		entries.append(split[0])
+		answers.append(split[1])
+	return entries, answers
 
 
 @app.route('/task1')
 def show_task_1():
-	entries = generate_scrambled(234)
+	entries, answers = parse_scrambled('task_1_words.txt')
+
 	return render_template('task_1.html', entries = entries)
 
 if __name__ == '__main__':
