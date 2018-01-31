17b8f2dfdc1baee80b4ffb9916e4278d531e4020
morenod <dmorenocuellar@gmail.com>
2018-01-31 10:18:40 -0500

    change to app

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
-app = Flask (__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
-	return "Hello World!"
+    return "Hello World!"
 
-@app.route('/')
+
+@app.route('/<name>')
 def hello_name(name):
-	return "Hello {}!".format(name)
+    return "Hello {}!".format(name)
 
-if __name__ == "__main__":
-	app.run()
+if __name__ == '__main__':
+    app.run()
